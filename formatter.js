f5141a4574fe54734eca0d6eb1656df75424ee58
Fabian Kackebartz <fabian.horvatic@gmail.com>
2022-03-12 15:53:23 +0100

    open top candle "wick"     //high top candle "Block"     //low bottom candle "block"     //close bottom canlde "wick"

--- formatter.js
+++ formatter.js
@@ -1,11 +1,21 @@
-//open top candle wick
-//close bottom canlde wick
-//high top candle 
-//low bottom candle
+//open top candle "wick"
+//high top candle "Block" 
+//low bottom candle "block"
+//close bottom canlde "wick"
 
 const dataframe = require("./DataFrame.json");
 const data = require("./data.json");
 
-
+/*
 console.log("Data[1]: ");
 console.log(data[1]);
+*/
+console.log("Block: " + 375 + " Open Top:" + data[375].open);
+
+for (let i = 0; i < data.length; i++) {
+    console.log("Block: " + i + " Open Top:" + data[i].open);
+
+
+
+
+}
